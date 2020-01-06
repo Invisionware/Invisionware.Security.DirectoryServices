c32de4adbe9c5480b1a8ade58266261bbe158798
ravensorb <sanderson@eye-catcher.com>
2020-01-06 17:16:58 -0500

    Updated build scripts

--- src/Invisionware.Security.DirectoryServices.LDAP/Extensions/DirectorySearcherExtensions.cs
+++ src/Invisionware.Security.DirectoryServices.LDAP/Extensions/DirectorySearcherExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Invisionware.Security.DirectoryServices.LDAP.Query;
+using System;
 using System.Collections.Generic;
 using System.DirectoryServices;
 using System.Text;
@@ -9,7 +10,20 @@ namespace Invisionware.Security.DirectoryServices.LDAP.Extensions
 	{
 		public static SearchResultCollection FindAllByUserName(this DirectorySearcher searcher, string accountFilder = "*", string givenNameFilter = "*", string surnameFilter = "*", string emailFilter = "*")
 		{
+			var filter = new LdapFilter(LdapQueryFilterBuilder.LdapFilterExpression.AND);
+			filter.Add(new LdapQueryFilterCondition("objectClass", LdapQueryFilterCondition.LdapFilterOperators.EQUALTO, "user"));
+			filter.Add(new LdapQueryFilterCondition("objectClass", LdapQueryFilterCondition.LdapFilterOperators.EQUALTO, "user"));
+
 			searcher.Filter = $"(&(objectClass=user)(&(sAMAccountName={accountFilder})(givenName={givenNameFilter})(sn={surnameFilter})(Mail={emailFilter})))";
+
+			return searcher.FindAll();
+		}
+
+		public static SearchResultCollection FindAll(this DirectorySearcher searcher, LdapFilter filter)
+		{
+			var filterString = LdapQueryFilterBuilder.GetFilter(filter, out string error);
+
+			searcher.Filter = filterString;
 
 			return searcher.FindAll();
 		}
